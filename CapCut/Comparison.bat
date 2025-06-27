04dc939c92243f0a6415ec908e0eab039936eaa1
Vitaly Yakutik <4yakudz@gmail.com>
2025-06-27 17:25:15 +0200

    Add .bat files to simplify CapCut run

---
+++ CapCut/Comparison.bat
@@ -0,0 +1,2 @@
+cd "%LocalAppData%\CapCut\User Data\Projects\com.lveditor.draft\Comparison"
+"%ProgramData%\Blackmagic Design\DaVinci Resolve\Fusion\Scripts\Utility\Capybara.ShotVideo.CapCut.Comparison.exe"
